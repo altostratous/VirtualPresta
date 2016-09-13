4551bc13f4bba735e38980966e3ace8a3edd8e30
altostratous <altostratous@gmail.com>
2016-09-13 08:32:28 +0430

    Implemented prestashop client.

--- VirtualPresta/PrestaShopClient.cs
+++ VirtualPresta/PrestaShopClient.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using CsvHelper;
 
 namespace VirtualPresta
 {
@@ -46,6 +47,15 @@ namespace VirtualPresta
             {
 
                 saveFileAndImages(product);
+            }
+        }
+
+        private void goToCSVImportForm()
+        {
+            while (!webDriver.Url.Contains("AdminImport"))
+            {
+                webDriver.Url = BaseAddress + "index.php?controller=AdminImport";
+                System.Threading.Thread.Sleep(100);
             }
         }
 
@@ -93,17 +103,138 @@ namespace VirtualPresta
 
         private void saveFileAndImages(Product product)
         {
-            webDriver.FindElement(By.Id("virtual_product")).Click();
-            webDriver.FindElement(By.Id("link-VirtualProduct")).Click();
-            webDriver.FindElement(By.CssSelector("label[for=is_virtual_file_on]")).Click();
-            webDriver.FindElement(By.Id("virtual_product_file_uploader")).SendKeys(product.File);
+            if (product.File != null && System.IO.File.Exists(product.File))
+            {
+                webDriver.FindElement(By.Id("virtual_product")).Click();
+                webDriver.FindElement(By.Id("link-VirtualProduct")).Click();
+                webDriver.FindElement(By.CssSelector("label[for=is_virtual_file_on]")).Click();
+                webDriver.FindElement(By.Id("virtual_product_file_uploader")).SendKeys(product.File);
+            }
 
+            if(product.ImageFiles != null)
+            {
+                webDriver.FindElement(By.Id("link-Images")).Click();
+                string files = "";
+                foreach (string imageFile in product.ImageFiles)
+                {
+                    if (System.IO.File.Exists(imageFile))
+                    {
+                        if (files != "")
+                        {
+                            files += ";";
+                        }
+                        files += imageFile;
+                    }
+                }
+                webDriver.FindElement(By.Id("file")).SendKeys(files);
+                webDriver.FindElement(By.Id("file-upload-button")).Click();
+            }
+
+            webDriver.FindElement(By.Id("link-VirtualProduct")).Click();
+
+            bool success = false;
+            while (!success)
+            {
+
+                var sumbitbuttons = webDriver.FindElements(By.Name("submitAddproductAndStay"));
+                foreach (IWebElement element in sumbitbuttons)
+                {
+                    try
+                    {
+                        element.Click();
+                        success = true;
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }
+            }
+            product.FileAndImagesSaved = true;
+        }
+
+        public IEnumerable<Product> GetProductsFromCSV(string csvFile)
+        {
+            CsvReader reader = new CsvReader(new System.IO.StreamReader(csvFile), new CsvHelper.Configuration.CsvConfiguration() { Delimiter = ";" });
+            while (reader.Read())
+            {
+                Product product = new Product() { Id = Convert.ToInt32(reader.GetField("ID")), Name = reader.GetField("Name *") };
+                if (reader.FieldHeaders.Contains("File"))
+                {
+                    product.File = reader.GetField("File");
+                }
+                if (reader.FieldHeaders.Contains("ImageFiles"))
+                {
+                    if (!string.IsNullOrEmpty(reader.GetField("ImageFiles")))
+                    {
+                        product.ImageFiles = new List<string>(reader.GetField("ImageFiles").Split(','));
+                    }
+                }
+                yield return product;
+
+            }
+            yield break;
+        }
+
+        private string getAppliedName(string filename)
+        {
+             return System.IO.Path.Combine(System.IO.Path.GetDirectoryName(filename), System.IO.Path.GetFileNameWithoutExtension(filename) + "applied.csv");
+        }
+
+        public void ApplyCSV(string csvFile, IEnumerable<Product> products = null)
+        {
+            if (products != null)
+            {
+                CsvReader reader = new CsvReader(new System.IO.StreamReader(csvFile), new CsvHelper.Configuration.CsvConfiguration() { Delimiter = ";" });
+                CsvWriter writer = new CsvWriter(new System.IO.StreamWriter(getAppliedName(csvFile)), new CsvHelper.Configuration.CsvConfiguration() { Delimiter = ";" });
+                reader.ReadHeader();
+                foreach (string header in reader.FieldHeaders)
+                {
+                    writer.WriteField(header);
+                }
+                writer.NextRecord();
+                foreach (Product product in products)
+                {
+                    writer.WriteField(product.Id);
+                    if (!reader.Read())
+                    {
+                        throw new Exception("No enough records in csv file.");
+                    }
+                    for (int i = 1; i < reader.FieldHeaders.Count(); i++)
+                    {
+                        writer.WriteField(reader.GetField(i));
+                    }
+                    writer.NextRecord();
+                }
+                if (reader.Read())
+                    throw new Exception("No enough products.");
+                writer.Dispose();
+                reader.Dispose();
+            }
+
+            goToCSVImportForm();
+
+            webDriver.FindElement(By.Id("entity")).SendKeys("محصول{ENTER}");
+            webDriver.FindElement(By.Id("file")).SendKeys(products != null ? getAppliedName(csvFile) : csvFile);
+            WebDriverWait wait = new WebDriverWait(webDriver, new TimeSpan(0, 0, 10));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.Id("csv_selected_filename")));
             bool success = false;
             while (!success)
             {
                 try
                 {
-                    webDriver.FindElement(By.Name("submitAddproductAndStay")).Click();
+                    webDriver.FindElement(By.Id("submitImportFile")).Click();
+                    success = true;
+                }
+                catch (Exception)
+                {
+                }
+            }
+            success = false;
+            while (!success)
+            {
+                try
+                {
+                    webDriver.FindElement(By.Id("import")).Click();
                     success = true;
                 }
                 catch (Exception)
